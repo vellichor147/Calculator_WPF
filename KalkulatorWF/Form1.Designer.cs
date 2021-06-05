74c29d3e0fbf7bda339e8eda3bf45c3b35025658
vellichor147 <71836410+vellichor147@users.noreply.github.com>
2021-06-05 11:31:14 +0200

    removed old code

--- KalkulatorWF/Form1.Designer.cs
+++ KalkulatorWF/Form1.Designer.cs
@@ -366,7 +366,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(323, 467);
+            this.ClientSize = new System.Drawing.Size(323, 472);
             this.Controls.Add(this.isPrefixDebug);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.isOpDebug);
--- KalkulatorWF/Form1.cs
+++ KalkulatorWF/Form1.cs
@@ -70,8 +70,8 @@ namespace KalkulatorWF
         {
             //briše vrijednosti svih varijabli
             Program.nullVariables();
+            //Null labels
             calculatorScreen.Text = null;
-            //Null debug labels
             num1Debug.Text = null;
             opDebug.Text = null;
             num2Debug.Text = null;
@@ -322,223 +322,42 @@ namespace KalkulatorWF
             if (e.KeyChar == '0')
             {
                 b0_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isOperator == false)
-                {
-                    Program.num1 += "0";
-                    Program.isPrefix = false;
-                    isPrefixDebug.Text = Convert.ToString(Program.isPrefix);
-                    calculatorScreen.Text = Program.num1;
-                    num1Debug.Text = Program.num1;
-                }
-                else
-                {
-                    Program.num2 += "0";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + "  " + Program.num2;
-                    num2Debug.Text = Program.num2;
-                }
-                */
-                #endregion
             }
             else if (e.KeyChar == '1')
             {
                 b1_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isFirst == true && Program.isOperator == false)
-                {
-                    Program.isPrefix = false;
-                    isPrefixDebug.Text = Convert.ToString(Program.isPrefix);
-                    Program.num1 += "1";
-                    calculatorScreen.Text = Program.num1;
-                    num1Debug.Text = Program.num1;
-                }
-                else
-                {
-
-                    Program.num2 += "1";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + " " + Program.num2;
-                    num2Debug.Text = Program.num2;
-                }
-                */
-                #endregion 
             }
             else if (e.KeyChar == '2')
             {
                 b2_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isOperator == false)
-                {
-                    Program.num1 += "2";
-                    Program.isPrefix = false;
-                    isPrefixDebug.Text = Convert.ToString(Program.isPrefix);
-                    calculatorScreen.Text = Program.num1;
-                    num1Debug.Text = Program.num1;
-                }
-                else
-                {
-                    Program.num2 += "2";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + "  " + Program.num2;
-                    num2Debug.Text = Program.num2;
-                }
-                */
-                #endregion region
             }
             else if (e.KeyChar == '3')
             {
                 b3_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isOperator == false)
-                {
-                    Program.num1 += "3";
-                    Program.isPrefix = false;
-                    isPrefixDebug.Text = Convert.ToString(Program.isPrefix);
-                    calculatorScreen.Text = Program.num1;
-                    num1Debug.Text = Program.num1;
-                }
-                else
-                {
-                    Program.num2 += "3";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + "  " + Program.num2;
-                    num2Debug.Text = Program.num2;
-                }
-                */
-                #endregion
             }
             else if (e.KeyChar == '4')
             {
                 b4_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isOperator == false)
-                {
-                    Program.num1 += "4";
-                    Program.isPrefix = false;
-                    isPrefixDebug.Text = Convert.ToString(Program.isPrefix);
-                    calculatorScreen.Text = Program.num1;
-                    num1Debug.Text = Program.num1;
-                }
-                else
-                {
-                    Program.num2 += "4";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + "  " + Program.num2;
-                    num2Debug.Text = Program.num2;
-                }
-                */
-                #endregion
             }
             else if (e.KeyChar == '5')
             {
                 b5_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isOperator == false)
-                {
-                    Program.num1 += "5";
-                    Program.isPrefix = false;
-                    isPrefixDebug.Text = Convert.ToString(Program.isPrefix);
-                    calculatorScreen.Text = Program.num1;
-                    num1Debug.Text = Program.num1;
-                }
-                else
-                {
-                    Program.num2 += "5";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + "  " + Program.num2;
-                    num2Debug.Text = Program.num2;
-                }
-                */
-                #endregion 
             }
             else if (e.KeyChar == '6')
             {
                 b6_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isOperator == false)
-                {
-                    Program.num1 += "6";
-                    Program.isPrefix = false;
-                    isPrefixDebug.Text = Convert.ToString(Program.isPrefix);
-                    calculatorScreen.Text = Program.num1;
-                    num1Debug.Text = Program.num1;
-                }
-                else
-                {
-                    Program.num2 += "6";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + "  " + Program.num2;
-                    num2Debug.Text = Program.num2;
-                }
-                */
-                #endregion
             }
             else if (e.KeyChar == '7')
             {
                 b7_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isOperator == false)
-                {
-                    Program.num1 += "7";
-                    Program.isPrefix = false;
-                    isPrefixDebug.Text = Convert.ToString(Program.isPrefix);
-                    calculatorScreen.Text = Program.num1;
-                    num1Debug.Text = Program.num1;
-                }
-                else
-                {
-                    Program.num2 += "7";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + "  " + Program.num2;
-                    num2Debug.Text = Program.num2;
-                }
-                */
-                #endregion
             }
             else if (e.KeyChar == '8')
             {
                 b8_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isOperator == false)
-                {
-                    Program.num1 += "8";
-                    Program.isPrefix = false;
-                    isPrefixDebug.Text = Convert.ToString(Program.isPrefix);
-                    calculatorScreen.Text = Program.num1;
-                    num1Debug.Text = Program.num1;
-                }
-                else
-                {
-                    Program.num2 += "8";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + "  " + Program.num2;
-                    num2Debug.Text = Program.num2;
-                }
-                */
-                #endregion
             }
             else if (e.KeyChar == '9')
             {
                 b9_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isOperator == false)
-                {
-                    Program.num1 += "9";
-                    Program.isPrefix = false;
-                    isPrefixDebug.Text = Convert.ToString(Program.isPrefix);
-                    calculatorScreen.Text = Program.num1;
-                    num1Debug.Text = Program.num1;
-                }
-                else
-                {
-                    Program.num2 += "9";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + "  " + Program.num2;
-                    num2Debug.Text = Program.num2;
-                }
-                */
-                #endregion
             }
             #endregion
 
@@ -546,70 +365,18 @@ namespace KalkulatorWF
             if (e.KeyChar == '+')
             {
                 add_button_Click(sender, e);
-                #region old code
-                /*
-                //e.Handled = true;
-                Program.op = "+";
-                Program.isOperator = true;
-                Program.isFirst = false;
-                calculatorScreen.Text = Program.num1 + " " + Program.op;
-                opDebug.Text = Program.op;
-                */
-                #endregion
             }
             else if (e.KeyChar == '-')
             {
                 sub_button_Click(sender, e);
-                #region old code
-                /*
-                //ako je prefix true a operator false to znači da niti jedan broj nije unesen i radi se o prefiksu prvog broja
-                if (Program.isPrefix == true && Program.isOperator == false)
-                {
-                    Program.num1 += "-";
-                    calculatorScreen.Text = Program.num1;
-                }
-                //kada kliknem minus prvo mora provjeriti da li sam prije toga kliknuo neki broj, jer ako jesam
-                //onda se radi o operatoru
-                else if (Program.isPrefix == false && Program.isOperator == false)
-                {
-                    Program.op = "-";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op;
-                    opDebug.Text = Program.op;
-                    Program.isOperator = true;
-                    isOpDebug.Text = Convert.ToString(Program.isOperator);
-                }
-                //ako je operator true znači da je operator unesen i da se sljedeći minus unosi kao predznak drugog broja
-                else if (Program.isSecondPrefix == true && Program.isOperator == true)
-                {
-                    Program.num2 += "-";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + "   " + Program.num2;
-                }
-                */
-                #endregion
             }
             else if (e.KeyChar == '*')
             {
                 multi_button_Click(sender, e);
-                #region old code
-                /*
-                Program.op = "*";
-                Program.isOperator = true;
-                calculatorScreen.Text = Program.num1 + " " + Program.op;
-                opDebug.Text = Program.op;
-                */
-                #endregion region
             }
             else if (e.KeyChar == '/')
             {
                 div_button_Click(sender, e);
-                #region old code
-                /*
-                Program.op = "/";
-                Program.isOperator = true;
-                calculatorScreen.Text = Program.num1 + " " + Program.op;
-                opDebug.Text = Program.op;
-                */
-                #endregion
             }
 
 
@@ -620,42 +387,10 @@ namespace KalkulatorWF
             if (e.KeyChar == '=')
             {
                 enter_button_Click(sender, e);
-                #region old code
-                /*
-                double newNum = Program.operationFunction(Program.num1, Program.num2, Program.op);
-                Program.num1 = Convert.ToString(newNum);
-                calculatorScreen.Text = Program.temp1 + " " + Program.op + " " + Program.num2 + " = " + Program.num1;
-                resDebug.Text = Program.num1;
-                num1Debug.Text = Program.num1;
-
-                opDebug.Text = null;
-                num2Debug.Text = null;
-
-                Program.isOperator = false;
-                Program.isPrefix = false;
-                Program.num2 = null;
-                Program.op = null;
-                */
-                #endregion
             }
             if (e.KeyChar == ',')
             {
                 com_button_Click(sender, e);
-                #region old code
-                /*
-                if (Program.isOperator == false)
-                {
-                    Program.num1 += ",";
-                    calculatorScreen.Text = Program.num1;
-
-                }
-                else
-                {
-                    Program.num2 += ",";
-                    calculatorScreen.Text = Program.num1 + " " + Program.op + " " + Program.num2;
-                }
-                */
-                #endregion
             }
             #endregion
         }
@@ -665,18 +400,6 @@ namespace KalkulatorWF
             if(e.KeyCode == Keys.Delete)
             {
                 del_button_Click(sender, e);
-                #region old code
-                /*
-                //briše vrijednosti svih varijabli
-                Program.nullVariables();
-                calculatorScreen.Text = null;
-                //Null debug labels
-                num1Debug.Text = null;
-                opDebug.Text = null;
-                num2Debug.Text = null;
-                resDebug.Text = null;
-                */
-                #endregion
             }
         }
         #endregion
